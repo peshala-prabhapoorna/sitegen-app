a0fbc75241a42b4d007b3072a4ad5283e8d87c12
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-07-02 08:45:59 +0530

    initial commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+
+def main():
+    node = TextNode("lavender is da best", "bold", "https://lavenderleit.dev")
+    print(node)
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_ineq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italic")
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This has a url", "link", "https://url.xyz")
+        self.assertEqual(
+            repr(node), 
+            "TextNode(This has a url, link, https://url.xyz)"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
