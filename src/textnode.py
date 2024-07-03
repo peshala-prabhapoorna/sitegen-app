23652c7621d399497931189348d8a1faca8218ad
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-07-03 16:58:36 +0530

    add TextNode to HTMLNode function

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,6 @@
+from htmlnode import LeafNode
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -15,3 +18,23 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("b", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode(
+                "img", "", {"src": text_node.url, "alt": text_node.text}
+            )
+        case _:
+            raise Exception(f"Inalid text type: {text_node.text_type}")
