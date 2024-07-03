8111655663c22c37ddc69fb917b69226ae5b96c3
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-07-03 20:47:29 +0530

    add split_nodes_delimiter and its unit tests, and add TextType

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,23 +1,83 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
     def test_ineq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "italic")
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
         self.assertNotEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This has a url", "link", "https://url.xyz")
+        node = TextNode("This has a url", TextType.LINK, "https://url.xyz")
         self.assertEqual(
-            repr(node), "TextNode(This has a url, link, https://url.xyz)"
+            repr(node),
+            "TextNode(This has a url, TextType.LINK, https://url.xyz)",
+        )
+
+
+class TestSplitNodeFn(unittest.TestCase):
+    def test_split_nodes_delimiter_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_delimiter([node], "`", TextType.CODE),
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_delimiter([node], "**", TextType.BOLD),
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_delimiter_italic(self):
+        node = TextNode("This is text with a *italicized* word", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_delimiter([node], "*", TextType.ITALIC),
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("italicized", TextType.ITALIC),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_delimiter_multi(self):
+        node1 = TextNode("This is text with a **bolded** word", TextType.TEXT)
+        node2 = TextNode("italicized text", TextType.ITALIC)
+        node3 = TextNode("link here", TextType.LINK, "https://boot.dev")
+        node4 = TextNode(
+            "This is another text with a **another bolded** word",
+            TextType.TEXT,
+        )
+        self.assertEqual(
+            split_nodes_delimiter(
+                [node1, node2, node3, node4], "**", TextType.BOLD
+            ),
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+                TextNode("italicized text", TextType.ITALIC),
+                TextNode("link here", TextType.LINK, "https://boot.dev"),
+                TextNode("This is another text with a ", TextType.TEXT),
+                TextNode("another bolded", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
         )
 
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,15 @@
+from enum import Enum
+
 from htmlnode import LeafNode
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 
 class TextNode:
@@ -22,19 +33,37 @@ class TextNode:
 
 def text_node_to_html_node(text_node):
     match text_node.text_type:
-        case "text":
+        case TextType.TEXT:
             return LeafNode(None, text_node.text)
-        case "bold":
+        case TextType.BOLD:
             return LeafNode("b", text_node.text)
-        case "italic":
-            return LeafNode("b", text_node.text)
-        case "code":
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
             return LeafNode("code", text_node.text)
-        case "link":
+        case TextType.LINK:
             return LeafNode("a", text_node.text, {"href": text_node.url})
-        case "image":
+        case TextType.IMAGE:
             return LeafNode(
                 "img", "", {"src": text_node.url, "alt": text_node.text}
             )
         case _:
             raise Exception(f"Inalid text type: {text_node.text_type}")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        text = node.text.split(delimiter)
+        new_nodes.extend(
+            [
+                TextNode(text[0], TextType.TEXT),
+                TextNode(text[1], text_type),
+                TextNode(text[2], TextType.TEXT),
+            ]
+        )
+
+    return new_nodes
