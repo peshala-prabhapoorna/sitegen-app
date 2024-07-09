f65ed869894ae403723e830084cc42606ec73e3b
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-07-09 23:56:17 +0530

    add fn to determine md block type, its tests, and BlockType class

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UL = "unordered_list"
+    OL = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -6,5 +19,22 @@ def markdown_to_blocks(markdown):
             continue
         block = block.strip()
         filtered_blocks.append(block)
-        
+
     return filtered_blocks
+
+
+def block_to_block_type(block):
+    if re.fullmatch(r"#{1,6} .*", block) is not None:
+        return BlockType.HEADING
+    if re.fullmatch(r"```.*```", block, flags=re.DOTALL) is not None:
+        return BlockType.CODE
+    if re.fullmatch(r"(>.*(\n|$))+", block) is not None:
+        return BlockType.QUOTE
+    if re.fullmatch(r"([*-] .*(\n|$))+", block) is not None:
+        return BlockType.UL
+    if re.fullmatch(r"(\d+\. .*(\n|$))+", block) is not None:
+        for i, line in enumerate(block.splitlines(), start=1):
+            if not line.startswith(f"{i}"):
+                return BlockType.PARAGRAPH
+        return BlockType.OL
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,10 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    BlockType,
+)
 
 
 class TestMarkdownBlocks(unittest.TestCase):
@@ -51,3 +55,19 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+
+class TestBlockToType(unittest.TestCase):
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), BlockType.UL)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OL)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
