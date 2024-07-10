ba3208d7d437265cc0c0406e7cf1184b537c35e0
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-07-10 21:13:00 +0530

    add fn to copy static files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
-from textnode import TextNode
+import os
+import shutil
+
+
+from copystatic import copy_dir
+
+
+static_path = "static"
+public_path = "public"
 
 
 def main():
-    node = TextNode("lavender is da best", "bold", "https://lavenderleit.dev")
-    print(node)
+    print("deleting public directory...")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+
+    print("copying static files to public directory...")
+    copy_dir(static_path, public_path)
 
 
 main()
